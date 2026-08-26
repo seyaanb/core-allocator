4ec7de9b3189e8d59253c2b240b6953f470aea4e
Seyaan Budhkar <seyaan.budhkar25@imperial.ac.uk>
2026-08-26 15:50:13 +0100

    Implement MemoryBlock class

---
+++ src/core/MemoryBlock.cpp
@@ -0,0 +1,32 @@
+#include "MemoryBlock.h"
+#include <sys/mman.h>
+#include <cstdlib>
+#include <cstdio>
+
+namespace core {
+
+MemoryBlock::MemoryBlock(size_t total_bytes) 
+    : m_base_ptr{ nullptr }, m_size{ total_bytes } {
+        m_base_ptr = mmap(nullptr, m_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
+        if (m_base_ptr == MAP_FAILED) {
+            fprintf(stderr, "Fatal: Memory map failed. Aborting.\n");
+            std::abort();
+        }
+}
+
+MemoryBlock::~MemoryBlock() {
+    if (m_base_ptr != nullptr && m_base_ptr != MAP_FAILED) {
+        (void)munmap(m_base_ptr, m_size);
+    }
+    
+}
+
+void* MemoryBlock::get_base_pointer() const noexcept {
+    return m_base_ptr;
+}
+
+size_t MemoryBlock::get_size() const noexcept {
+    return m_size;
+}
+
+}
